b714f49bfc7186d79fa471b581cb15b5b529b1c1
Nicholas Becker <nicholas.becker@d25s70ndgq18m.oc.edu>
2016-11-14 21:42:48 -0600

    all done I think

---
+++ iTrello/iTrello/CardDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CardDetailViewController.swift
+//  iTrello
+//
+//  Created by Nicholas Becker on 11/14/16.
+//  Copyright © 2016 ismoresimpler. All rights reserved.
+//
+
+import Foundation
